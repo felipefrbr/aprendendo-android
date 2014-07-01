ebd0a95c265abdea5eac74f8035837eef474f6f8
felipefrbr <felipefrbr@gmail.com>
2014-07-01 09:20:36 -0300

    Nova Viagem e Novo Gasto

---
+++ BoaViagem/src/br/com/casadocodigo/boaviagem/activity/ViagemActivity.java
@@ -0,0 +1,14 @@
+package br.com.casadocodigo.boaviagem.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import br.com.casadocodigo.boaviagem.R;
+
+public class ViagemActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.nova_viagem);
+	}
+}
